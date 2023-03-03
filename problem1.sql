75313bcf140c3da43758a1ff73d1be35ed181371
anu-ban <wwr350i@gmail.com>
2023-03-03 07:25:40 +0000

    Commited

---
+++ problem1.sql
@@ -0,0 +1,7 @@
+INSERT INTO song(title,artist)
+VALUES('Let it be' ,'Beatles');
+INSERT INTO song(title,artist)
+VALUES('Hotel California' ,'Eagles');
+
+INSERT INTO song(title,artist)
+VALUES('Kashmir' ,'Led Zeppelin');
